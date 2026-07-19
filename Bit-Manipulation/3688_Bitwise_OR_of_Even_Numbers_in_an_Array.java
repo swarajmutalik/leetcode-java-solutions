cf75dcf775bc5a633c96216f4618d75ac4aa1f2b
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2026-07-20 01:14:20 +0530

    Implement method to compute OR of even numbers

---
+++ Bit-Manipulation/3688_Bitwise_OR_of_Even_Numbers_in_an_Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int evenNumberBitwiseORs(int[] nums) {
+        int result = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] % 2 == 0){
+                result = result | nums[i];
+            }
+
+        }
+        return result;
+    }
+}
