7ea40ed1958937fdc717b2ba3ba2c33575a338a0
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2026-07-21 15:58:40 +0530

    Add solution for validating perfect squares

---
+++ Binary-Search/367_Valid_Perfect_Square.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        for(long i = 0; i <= num; i++){
+            long square = i * i;
+
+            if(square == num){
+                return true;
+            }
+            else if(square > num){
+                return false;
+            }
+        }
+        return false;
+    }
+}
