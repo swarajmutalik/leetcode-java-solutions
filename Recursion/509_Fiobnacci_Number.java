056fe319c22ac781b160b619e63ce500e92a140c
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2026-07-22 22:11:43 +0530

    Implement Fibonacci number calculation using iteration

---
+++ Recursion/509_Fiobnacci_Number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        
+        int a = 0;
+        int b = 1;
+        int count = 2;
+
+        while(count <= n){
+            int temp = b;
+            b  = b + a;
+            a = temp;
+            count++;
+        }
+        return b;
+    }
+}
